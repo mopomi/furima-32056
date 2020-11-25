ec313a09cb57652ebd656c26c91fda1f65f67cdf
mopomi <mrooooo.33@gmail.com>
2020-11-25 19:22:08 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
           validates :name, presence: true
-          validates :last_name, presence: true
-          validates :first_name, presence: true
-          validates :last_name_kana, presence: true
-          validates :first_name_kana, presence: true
+          validates :last_name, presence: true,
+                     format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, 
+                     message: 'Full-width characters' } 
+
+          validates :first_name, presence: true,
+                     format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, 
+                     message: 'Full-width characters' } 
+
+          validates :last_name_kana, presence: true, 
+                     format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, 
+                     message:'Full-width katakana characters' }
+
+          validates :first_name_kana, presence: true, 
+                     format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/,
+                     message:'Full-width katakana characters' }
           validates :birthday, presence: true
-          
+
+          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d_-]+\z/i
+          validates :password, presence: true,
+                     format: { with: VALID_PASSWORD_REGEX }
 end
