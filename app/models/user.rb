49c51ad5e9cce2f2c1cb6f0a85015ef50254ea8e
mopomi <mrooooo.33@gmail.com>
2020-11-26 12:32:14 +0900

    テストコード追加・修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
-  validates :last_name, presence: true,
-                        format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,
-                                  message: 'Full-width characters' }
-
-  validates :first_name, presence: true,
-                         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,
-                                   message: 'Full-width characters' }
-
-  validates :last_name_kana, presence: true,
-                             format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/,
-                                       message: 'Full-width katakana characters' }
-
-  validates :first_name_kana, presence: true,
-                              format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/,
-                                        message: 'Full-width katakana characters' }
-  validates :birthday, presence: true
+  with_options presence: true do
+     validates :name
+     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+     validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+     validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' }
+     validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' }
+     validates :birthday
+  end
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d_-]+\z/i.freeze
   validates :password, presence: true,
