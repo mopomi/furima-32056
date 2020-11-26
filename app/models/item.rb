b36a9e5b4b441c4988fbad20ee622c3b41b53226
mopomi <mrooooo.33@gmail.com>
2020-11-26 20:13:19 +0900

    出品機能実装1

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image
     validates :title
     validates :introduction
     validates :price
