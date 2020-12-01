60a9a6ce19bc078bc57742cc9a3e12fa848f2250
mopomi <mrooooo.33@gmail.com>
2020-12-01 16:07:41 +0900

    商品購入機能単体テストコード

--- app/models/address_purchase.rb
+++ app/models/address_purchase.rb
@@ -6,6 +6,7 @@ class AddressPurchase
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
+    validates :house_number
     validates :phone_number, format: {with: /\A\d{1,11}\z/}
     validates :token
   end
